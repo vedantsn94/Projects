e4178d9a88d119f2255c4b9892eed0061ca3d12c
Vedant Nichite <vedantnichite121@gmail.com>
2026-01-13 14:22:33 +0530

    Add files via upload

---
+++ Prime/script.js
@@ -0,0 +1,6 @@
+// Minimal JS (expand later)
+document.querySelectorAll('.movie-row img').forEach(img => {
+  img.addEventListener('click', () => {
+    alert('Video playback not implemented');
+  });
+});
